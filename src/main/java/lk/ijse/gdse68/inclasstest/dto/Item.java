e158f2dbbad68580c26adb2584604e2fd0e79134
MalithShehan <shehansandakalum2003@gmail.com>
2024-09-01 10:17:21 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse68/inclasstest/dto/Item.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse68.inclasstest.dto;
+
+public class Item {
+    private String ChickenRoll;
+    private String FishPastry;
+    private String FishBus;
+    private String ChickenBun;
+
+
+}
